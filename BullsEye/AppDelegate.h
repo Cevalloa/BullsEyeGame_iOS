5ca5c06a9762be8d158c56b1d81d171455a55bc1
Cevalloa <alexcevallos@Alex-Cevalloss-MacBook-Pro.local>
2014-01-31 21:23:43 -0500

    Initial Commit

---
+++ BullsEye/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BullsEye
+//
+//  Created by Alex Cevallos on 1/31/14.
+//  Copyright (c) 2014 com.AlexCevallos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
